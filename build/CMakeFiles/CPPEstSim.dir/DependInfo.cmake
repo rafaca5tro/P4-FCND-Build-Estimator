4f2c3531fc9918aff3a81c3b8b5bcb22fa6697e2
Rafael Castro <rcgonzsv@aol.com>
2021-01-30 15:31:24 -0600

    first commit

---
+++ build/CMakeFiles/CPPEstSim.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/BaseController.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/BaseController.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/BaseQuadEstimator.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/BaseQuadEstimator.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Drawing/ColorUtils.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Drawing/ColorUtils.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Drawing/DrawingFuncs.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Drawing/DrawingFuncs.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Drawing/GLUTMenu.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Drawing/GLUTMenu.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Drawing/Graph.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Drawing/Graph.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Drawing/GraphManager.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Drawing/GraphManager.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Drawing/Visualizer_GLUT.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Drawing/Visualizer_GLUT.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Math/Geometry.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Math/Geometry.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Math/Random.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Math/Random.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/MavlinkNode/MavlinkNode.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/MavlinkNode/MavlinkNode.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/MavlinkNode/MavlinkTranslation.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/MavlinkNode/MavlinkTranslation.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/MavlinkNode/PracticalSocket.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/MavlinkNode/PracticalSocket.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/QuadControl.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/QuadControl.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/QuadEstimatorEKF.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/QuadEstimatorEKF.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Simulation/BaseDynamics.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Simulation/BaseDynamics.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Simulation/QuadDynamics.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Simulation/QuadDynamics.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Simulation/Simulator.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Simulation/Simulator.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Simulation/magnetometer.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Simulation/magnetometer.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Simulation/opticalflow.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Simulation/opticalflow.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Simulation/rangefinder.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Simulation/rangefinder.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Trajectory.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Trajectory.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Utility/Camera.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Utility/Camera.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Utility/SimpleConfig.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Utility/SimpleConfig.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/Utility/Timer.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/Utility/Timer.cpp.o"
+  "/Users/rcastro/Documents/FCND-Estimation-CPP/src/main.cpp" "/Users/rcastro/Documents/FCND-Estimation-CPP/build/CMakeFiles/CPPEstSim.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../lib"
+  "/Library/Developer/CommandLineTools/SDKs/MacOSX11.0.sdk/System/Library/Frameworks/GLUT.framework/Headers"
+  "/Library/Developer/CommandLineTools/SDKs/MacOSX11.0.sdk/System/Library/Frameworks/OpenGL.framework"
+  "/Users/rcastro/anaconda3/include/qt"
+  "/Users/rcastro/anaconda3/include/qt/QtCore"
+  "/Users/rcastro/anaconda3/./mkspecs/macx-clang"
+  "/Users/rcastro/anaconda3/include/qt/QtNetwork"
+  "/Users/rcastro/anaconda3/include/qt/QtWidgets"
+  "/Users/rcastro/anaconda3/include/qt/QtGui"
+  "/Library/Developer/CommandLineTools/SDKs/MacOSX11.0.sdk/System/Library/Frameworks/OpenGL.framework/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
